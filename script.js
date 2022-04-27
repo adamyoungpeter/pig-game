b8465e202a8d6986d60c7101899a3de391ae5de1
Adam Youngpeter <adamyoungpeter@gmail.com>
2022-04-27 11:11:28 -0400

    added selectors and dice rolling functionality

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
 'use strict';
+
+// Selecting Elements
+const score0El = document.getElementById('score--0');
+const score1El = document.getElementById('score--1');
+const diceEl = document.querySelector('.dice');
+
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
+
+// Starting Conditions
+score0El.textContent = 0;
+score1El.textContent = 0;
+diceEl.classList.add('hidden');
+
+// Rolling Dice Functionality
+btnRoll.addEventListener('click', function () {
+  // 1. Generate random dice roll
+  const diceValue = Math.trunc(Math.random() * 6) + 1;
+
+  // 2. Display dice
+  diceEl.classList.remove('hidden');
+  diceEl.src = `dice-${diceValue}.png`;
+
+  // 3. Check if one was rolled: If so, change players
+  //   if (diceValue === 1) {
+  //   }
+});
