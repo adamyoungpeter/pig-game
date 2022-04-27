17df4a46104e554265dcd84088680ecfb639c893
Adam Youngpeter <adamyoungpeter@gmail.com>
2022-04-27 15:30:42 -0400

    added complete game functionality and extracted logic to functions called by click methods

--- script.js
+++ script.js
@@ -1,29 +1,91 @@
 'use strict';
 
 // Selecting Elements
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
 const score0El = document.getElementById('score--0');
 const score1El = document.getElementById('score--1');
-const diceEl = document.querySelector('.dice');
+const currentPlayer0Score = document.getElementById('current--0');
+const currentPlayer1Score = document.getElementById('current--1');
 
+const diceEl = document.querySelector('.dice');
 const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
 
-// Starting Conditions
-score0El.textContent = 0;
-score1El.textContent = 0;
-diceEl.classList.add('hidden');
+let scores, currentScore, activePlayer, playing;
 
-// Rolling Dice Functionality
-btnRoll.addEventListener('click', function () {
-  // 1. Generate random dice roll
-  const diceValue = Math.trunc(Math.random() * 6) + 1;
+const newGame = function () {
+  scores = [0, 0];
+  currentScore = 0;
+  activePlayer = 0;
+  playing = true;
 
-  // 2. Display dice
-  diceEl.classList.remove('hidden');
-  diceEl.src = `dice-${diceValue}.png`;
+  score0El.textContent = 0;
+  score1El.textContent = 0;
+  currentPlayer0Score.textContent = 0;
+  currentPlayer1Score.textContent = 0;
 
-  // 3. Check if one was rolled: If so, change players
-  //   if (diceValue === 1) {
-  //   }
-});
+  diceEl.classList.add('hidden');
+  player0El.classList.remove('player--winner');
+  player1El.classList.remove('player--winner');
+  player0El.classList.add('player--active');
+  player1El.classList.remove('player--active');
+};
+newGame();
+
+const clearCurrentPlayerScore = function () {
+  document.getElementById(`current--${activePlayer}`).textContent = 0;
+};
+
+const toggleActive = function () {
+  clearCurrentPlayerScore();
+  currentScore = 0;
+  activePlayer = activePlayer === 0 ? 1 : 0;
+  player0El.classList.toggle('player--active');
+  player1El.classList.toggle('player--active');
+};
+
+const rollLogic = function () {
+  if (playing) {
+    const diceValue = Math.trunc(Math.random() * 6) + 1;
+    diceEl.classList.remove('hidden');
+    diceEl.src = `dice-${diceValue}.png`;
+
+    if (diceValue !== 1) {
+      currentScore += diceValue;
+      document.getElementById(`current--${activePlayer}`).textContent =
+        currentScore;
+    } else {
+      toggleActive();
+    }
+  }
+};
+
+const holdScore = function () {
+  if (playing) {
+    scores[activePlayer] += currentScore;
+    document.getElementById(`score--${activePlayer}`).textContent =
+      scores[activePlayer];
+
+    if (scores[activePlayer] >= 100) {
+      playing = false;
+      diceEl.classList.add('hidden');
+
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.add('player--winner');
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.remove('player--active');
+    } else {
+      toggleActive();
+    }
+  }
+};
+
+btnRoll.addEventListener('click', rollLogic);
+
+btnHold.addEventListener('click', holdScore);
+
+btnNew.addEventListener('click', newGame);
